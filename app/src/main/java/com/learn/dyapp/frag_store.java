85f138136b5d2d0f18d9b73bbebb73511bb5508b
Code0987 <durgapalneeraj@gmail.com>
2017-02-09 00:18:28 +0530

    fixes

--- app/src/main/java/com/learn/dyapp/frag_store.java
+++ app/src/main/java/com/learn/dyapp/frag_store.java
@@ -1,24 +1,31 @@
 package com.learn.dyapp;
 
-
 import android.app.Activity;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 
-public class frag_store extends Activity{
-
-
+public class frag_store extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frag_store);
 
+        // Create new fragment and transaction
+        Fragment newFragment = new calc_frag();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        // getSupportFragmentManager - for app compat
+        // getFragmentManager - normal activity
 
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack if needed
+        transaction.replace(R.id.fragment_container, newFragment);
+        transaction.addToBackStack(null);
 
+        // Commit the transaction
+        transaction.commit();
     }
+
 }
