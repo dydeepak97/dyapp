95c1a931d8b4a1c50ca82f229652f7a40e1ae0a7
dydeepak97 <dydeepak97@gmail.com>
2017-02-08 18:32:42 +0530

    Initial commit

---
+++ app/src/main/java/com/learn/dyapp/MainActivityList.java
@@ -0,0 +1,49 @@
+package com.learn.dyapp;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.view.Menu;
+
+public class MainActivityList extends Activity {
+
+    Button but_submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_list);
+        //Storing
+        String[] mobileArray= getResources().getStringArray(R.array.ABC);
+
+        //Binding
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.list_item,mobileArray);
+
+        //Displaying
+        ListView listView= (ListView) findViewById(R.id.mobile_list);
+        listView.setAdapter(adapter);
+
+
+
+
+
+
+
+}
+    //when  button clicked
+    public void sendf(View v){
+
+
+        Intent i= new Intent(getApplicationContext(),frag_store.class);
+        startActivity(i);
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/learn/dyapp/frag_store.java
@@ -0,0 +1,24 @@
+package com.learn.dyapp;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class frag_store extends Activity{
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frag_store);
+
+
+
+    }
+}
